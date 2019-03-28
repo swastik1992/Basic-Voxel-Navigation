5304305bf9cc1848451546d673f8c1ebbabd155c
swastik1992 <swastikswarnkar@gmail.com>
2019-03-28 23:45:46 +0800

    Project Setup.

---
+++ VoxelNavigation/Source/VoxelNavigation/VoxelNavigationGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "VoxelNavigationGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VOXELNAVIGATION_API AVoxelNavigationGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
